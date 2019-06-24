d835d55c74bbe788dc6c46b8d07e39d3d5efa940
Mahmoud Rekapi <m.rekapi@gmail.com>
2019-06-24 15:59:59 +0200

    Java Basic exercises
    this will be 100 exercise to cover all java basics

---
+++ src/Exercises.java
@@ -0,0 +1,95 @@
+public class Exercises{
+
+	/* 1. write a Program takes number as input and prints its multiplication table up to 10 
+		  input : 2
+		  output : 
+		  	2x1 = 2
+			2x2 = 4
+			2x3 = 6
+			2x4 = 8
+			2x5 = 10
+			......
+			2x10 = 20
+		--------------------------------------------------------------------------------------
+		2. using for loop to draw the american flag 
+		output : ******============
+				 ******============
+				 ******============
+				 ******============
+				 ==================
+				 ==================
+				 ==================
+				 ==================			
+		--------------------------------------------------------------------------------------
+		3. Reversing String 
+		input : Kareem 
+		output : meeraK
+		--------------------------------------------------------------------------------------
+		4. compute the sum of the digits of an integer 
+		input : 15
+		output : 6
+		--------------------------------------------------------------------------------------
+		5. count the letters, spaces, numbers and other characters in an input string
+		input : my name is m@7moud
+		output : 
+		Number of letters : 5
+		Number of spaces : 0
+		Number of numbers : 1
+		Number of characters : 1 
+		--------------------------------------------------------------------------------------
+		6. Calculate the Parameter and Area of Circle 
+		input : 5
+		output : 
+		parameter = 31.415926
+		Area = 78.5398163
+		--------------------------------------------------------------------------------------
+		7. check the input from the user is Even or Odd
+		input : 5
+		output : this is odd number 
+		--------------------------------------------------------------------------------------
+		8. converting from decimal number into binary number 
+		input : 30
+		output : 11110
+		--------------------------------------------------------------------------------------
+		9. write a program to write your password with hidden character 
+		--------------------------------------------------------------------------------------
+		10. write a program to captalize the first letter on a sentence 
+		input : quick brown fox jump over lazy dog 
+		output : Quick Brown Fox Jump Over Lazy Dog
+		--------------------------------------------------------------------------------------
+		11. write a program test if the first and the last element of an array are the same of the length of the array 
+		--------------------------------------------------------------------------------------
+		12. write a program create new array with length of 2 from two arrays of integers with 3 elements and the new array will have the first 
+			element of array 1 and the last number of array 2 
+		input : array1 = [50,20,30]
+				array2 = [15,80,24]
+		output : newArray = [50,24]		
+		--------------------------------------------------------------------------------------
+		13. write a program to rotate an array (length 3) of integers 
+		input : [20,30,40]
+		output : [30,40,20]
+		--------------------------------------------------------------------------------------
+		14. write a program to find the largest number between first, last and medium elements in an array 
+		input : [20,30,40,50,67]
+		output : the largest number = 67
+		--------------------------------------------------------------------------------------
+		15. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	*/
+
+
+
+}
